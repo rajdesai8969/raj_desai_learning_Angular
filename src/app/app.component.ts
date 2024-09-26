3efc8b5e7c0bc04067d383e759d792e2fe975ffb
raj desai <156331263+rayka8969@users.noreply.github.com>
2024-09-25 22:47:22 -0400

    added the css and completed all the steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Car} from "../model/Car";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'raj-desai-learning-Angular';
-  car:string='i have a nice car';
-  bike:string='i dont have a bike';
+
+  car1 : Car ={Name : "BMW", Model : "X5" , Color : "Black", Make:"German", Year: 2023, isAdmin : true};
+  car2 : Car ={Name : "Audi", Model : "Rs7" , Color : "Blue", Make:"German", Year: 2014, isAdmin : true};
+  car3 : Car ={Name : "Lexus", Model : "LFA" , Color : "Red", Make:"Japan", Year: 2018, isAdmin : false};
+  car4 : Car ={Name : "Hyundai", Model : "Elantra" , Color : "White", Make:"Korea", Year: 2019, isAdmin : true};
+  car5 : Car ={Name : "Honda", Model : "Civic" , Color : "Green", Make:"Japan", Year: 2021, isAdmin : false};
+  car6 : Car ={Name : "Dodge", Model : "Charger" , Color : "Yellow", Make:"USA", Year: 2020, isAdmin : false};
+
+  carList: Car[]=[this.car1,this.car2,this.car3,this.car4,this.car5,this.car6]
+
 }
