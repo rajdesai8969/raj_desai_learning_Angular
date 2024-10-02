2cd5d8d7cd726cfb2006fb5805ac663a07990492
raj desai <156331263+rayka8969@users.noreply.github.com>
2024-10-02 17:36:18 -0400

    created two new components named car-list and car-list-item.

---
+++ src/app/car-list-item/car-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './car-list-item.component.html',
+  styleUrl: './car-list-item.component.css'
+})
+export class CarListItemComponent {
+
+}
