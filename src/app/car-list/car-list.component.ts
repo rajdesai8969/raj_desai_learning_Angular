17b069953b67708af673d666c8e9290bb5ee8cad
raj desai <156331263+rayka8969@users.noreply.github.com>
2024-10-10 15:47:03 -0400

    step 4 and 5  completed

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -15,11 +15,7 @@ import {NgClass, NgForOf} from "@angular/common";
   styleUrl: './car-list.component.css'
 })
 export class CarListComponent {
-  car1 : Car ={Name : "BMW", Model : "X5" , Color : "Black", Make:"German", Year: 2023, isAdmin : true};
-  car2 : Car ={Name : "Audi", Model : "Rs7" , Color : "Blue", Make:"German", Year: 2014, isAdmin : true};
-  car3 : Car ={Name : "Lexus", Model : "LFA" , Color : "Red", Make:"Japan", Year: 2018, isAdmin : false};
-  car4 : Car ={Name : "Hyundai", Model : "Elantra" , Color : "White", Make:"Korea", Year: 2019, isAdmin : true};
 
 
-  carList: Car[]=[this.car1,this.car2,this.car3,this.car4]
+  carList: Car[]=[]
 }
