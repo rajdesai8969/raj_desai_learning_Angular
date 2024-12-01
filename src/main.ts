d6cac24c0b99d377daaf2a22f605dc9d6d7918af
raj desai <156331263+rayka8969@users.noreply.github.com>
2024-12-01 15:37:09 -0500

    completed assignment 8

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import {provideRouter, Routes} from "@angular/router";
-import {CarListItemComponent} from "./app/car-list-item/car-list-item.component";
 import {CarListComponent} from "./app/car-list/car-list.component";
-import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
-import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
+
 
 const routes: Routes = [
   {path:'', redirectTo: '/CarListComponent', pathMatch: 'full'},
